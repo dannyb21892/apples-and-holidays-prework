feecd8a1116a3e8a6fd3096c4226657d32203fb8
Daniel Baamonde <dannyb21892@gmail.com>
2018-02-13 16:42:22 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.to_s.capitalize}:"
     
     holidays.each do |holiday, supplies|
-      holidaywords = holiday.to_s.split
+      holidaywords = holiday.to_s.split("_")
       holidaywords.each do |word|
         word.capitalize!
       end
