098b2d1df54be79e3e921dcf6e33a928f0bc26d4
Daniel Baamonde <dannyb21892@gmail.com>
2018-02-13 16:38:54 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,7 +50,7 @@ def all_winter_holiday_supplies(holiday_hash)
   holiday_hash[:winter].each do |key, value|
     values << value
   end
-  value.flatten
+  values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
